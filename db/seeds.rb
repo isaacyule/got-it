26b80c583daedfa76d92ad4c55d7b9d2d0c01cfe
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2017-11-13 15:12:57 +0000

    updated seed to conform with the schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,6 @@ require 'faker'
 
   first_name    = user['results'][0]['name']['first']
   last_name     = user['results'][0]['name']['last']
-  username      = user['results'][0]['login']['username']
   password      = user['results'][0]['login']['password']
   email         = user['results'][0]['email']
   profile_photo = user['results'][0]['picture']["large"]
@@ -31,5 +30,5 @@ require 'faker'
   phone         = user['results'][0]['phone']
   registered    = user['results'][0]['registered']
 
-  user.new(first_name: first_name, last_name: last_name, username: username, password: password, email: email, profile_photo: profile_photo, profile_text: profile_text, street: street, town: town, state: state, postcode: postcode, country: country, phone: phone, registered: registed)
+  user.new(first_name: first_name, last_name: last_name, password: password, email: email, profile_photo: profile_photo, profile_text: profile_text, street: street, town: town, state: state, postcode: postcode, country: country, phone: phone, registered: registered)
 }
