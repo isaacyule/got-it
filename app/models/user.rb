99aa6156640b35eb4cd712d49985a7d2ab58c209
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2017-11-13 18:28:06 +0000

    removed arbitrary users controller in liue of devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :products
   has_many :reviews
-  validates :first_name, :last_name, :email, :password, presence: true
+  # validates :first_name, :last_name, :email, :password, presence: true
   # TO DO: mount_uploader :photo, PhotoUploader
 
   devise :database_authenticatable, :registerable,
