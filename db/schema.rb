565c12d7c6bff98d1547984691af6359ce5b12a5
isaac yule <isaac.k.yule@gmail.com>
2017-11-13 16:22:05 +0000

    tested products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113145312) do
+ActiveRecord::Schema.define(version: 20171113154640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(version: 20171113145312) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "profile_photo"
+    t.string "profile_text"
+    t.string "street"
+    t.string "town"
+    t.string "postcode"
+    t.string "country"
+    t.string "phone"
+    t.string "registration_date"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
