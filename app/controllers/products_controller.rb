a035f7aee08e9eb525a40f876b6dfe15e09fc7c2
Vaiva Margeleviciute <vaivav11@gmail.com>
2017-11-14 12:07:12 +0000

    added photo uploader ans views, exept index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,9 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:index, :show, :create, :edit, :destroy]
+  before_action :set_user
 
   def index
-
+    @products = Products.all
   end
 
   def show
@@ -40,11 +41,15 @@ class ProductsController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
   def set_product
     @product = Product.find(params[:id])
   end
 
   def product_params
-    params.require(product).permit(:name, :description, :price_per_day, :deposit, :minimum_fee)
+    params.require(product).permit(:name, :description, :price_per_day, :deposit, :minimum_fee, :photo)
   end
 end
