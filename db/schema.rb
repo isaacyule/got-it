a8756099fa92cef8c0061b2a264e36f67d6e8f7b
isaac yule <isaac.k.yule@gmail.com>
2017-11-15 11:53:02 +0000

    created request controller, implemented new/create actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114161810) do
+ActiveRecord::Schema.define(version: 20171115114929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,11 +32,11 @@ ActiveRecord::Schema.define(version: 20171114161810) do
     t.bigint "user_id"
     t.bigint "product_id"
     t.string "status"
-    t.string "start_date"
-    t.string "end_date"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["product_id"], name: "index_requests_on_product_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
