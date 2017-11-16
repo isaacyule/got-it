eca4f2712b539b2a09e3229b54c448569b9b8ca9
isaac yule <isaac.k.yule@gmail.com>
2017-11-16 13:42:10 +0000

    added status to requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115140326) do
+ActiveRecord::Schema.define(version: 20171116133639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,12 +34,12 @@ ActiveRecord::Schema.define(version: 20171115140326) do
   create_table "requests", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "product_id"
-    t.string "status"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "start_date"
     t.date "end_date"
+    t.string "status", default: "Pending"
     t.index ["product_id"], name: "index_requests_on_product_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
   end
