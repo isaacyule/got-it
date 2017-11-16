174f65be110bde31d8e0f40cd19a278ed140eec7
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2017-11-16 18:25:38 +0000

    hotfixed card search

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,8 +1,8 @@
-AlgoliaSearch.configuration = { application_id: '3QRXVE4VDT', api_key: ENV[algolia_api_key] }
+AlgoliaSearch.configuration = { application_id: '3QRXVE4VDT', api_key: ENV['algolia_api_key'] }
 
 AlgoliaSearch.configuration = {
   application_id: '3QRXVE4VDT',
-  api_key: ENV[algolia_api_key],
+  api_key: ENV['algolia_api_key'],
   connect_timeout: 2,
   receive_timeout: 30,
   send_timeout: 30,
