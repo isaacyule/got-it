fc679d30bd570b84707d6f1bf2fc13042381f5e8
isaac yule <isaac.k.yule@gmail.com>
2017-11-17 11:58:04 +0000

    added review create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,21 @@
 class ReviewsController < ApplicationController
 
   def create
-    @product = "HELLO!"
+    @product = Product.find(params[:product_id])
+    @request = Request.where(product: @product, user: current_user).first
+    @review = Review.new(review_params)
+    @review.request = @request
+    @review.user = current_user
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render 'product/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
