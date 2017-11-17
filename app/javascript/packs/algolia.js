af309c08d182b984886301eb98d4a327b02e1087
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2017-11-17 12:21:34 +0000

    filter index search to show nearest produt first

--- app/javascript/packs/algolia.js
+++ app/javascript/packs/algolia.js
@@ -23,38 +23,35 @@ const algoliaSearch = () => {
   container.innerHTML = "";
   query = form.value;
   // query the algolia api for search results
-  index.search(query, function searchDone(err, content) {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  index.search(query, {aroundLatLngViaIP: true }).then(
+    function searchDone(content) {
 
-    // updates the counter at the top of the page
-    counter = document.getElementById('counter');
-    counter.innerHTML = `<h1>${content.hits.length} Search results in your area...</h1>`;
+      // updates the counter at the top of the page
+      counter = document.getElementById('counter');
+      counter.innerHTML = `<h1>${content.hits.length} Search results in your area...</h1>`;
 
-    content.hits.forEach( product => {
-      // Appends a new card to the cards container for every search result.
-      container.insertAdjacentHTML('beforeend', `
-        <div class="col-xs-12 col-sm-6">
-          <div class="card">
-            <a class='link-to-product' href='http://localhost:3000/products/${product.objectID}/'>
-              <div class='card-body' style='background-image: url(${product['photo']})'></div>
-              <div class="card-footer">
-                <div class="container">
-                  <div class="row">
-                    <div class="col-xs-12 col-lg-6">
-                      <span class="description">${product["name"]}</span>
-                    </div>
-                    <div class="col-xs-12 col-lg-6">
-                      <span class="price">£${product["price_per_day"]}/day</span>
+      content.hits.forEach(product => {
+        // Appends a new card to the cards container for every search result.
+        container.insertAdjacentHTML('beforeend', `
+          <div class="col-xs-12 col-sm-6">
+            <div class="card">
+              <a class='link-to-product' href='http://localhost:3000/products/${product.objectID}/'>
+                <div class='card-body' style='background-image: url(${product['photo']})'></div>
+                <div class="card-footer">
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-xs-12 col-lg-6">
+                        <span class="description">${product["name"]}</span>
+                      </div>
+                      <div class="col-xs-12 col-lg-6">
+                        <span class="price">£${product["price_per_day"]}/day</span>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          </div>
-        </div>`);
+              </a>
+            </div>
+          </div>`);
     });
   });
 };
