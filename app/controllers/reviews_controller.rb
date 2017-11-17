9ed61d7ba3e46ad243a35ed63537f3ba1bd8009e
Vaiva Margeleviciute <vaivav11@gmail.com>
2017-11-17 12:02:20 +0000

    .

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,22 @@
 class ReviewsController < ApplicationController
+  before_action :review_params, only: [:create]
 
   def create
-    @product = "HELLO!"
+    @product = Product.find(params[:product_id])
+    @request = Request.where(product: @product, user: current_user).first
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.request = @request
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render 'products/show'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
