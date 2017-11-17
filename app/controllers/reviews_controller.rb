343302eb96a3300f52f783128bf3850f847954f2
Vaiva Margeleviciute <vaivav11@gmail.com>
2017-11-17 12:15:22 +0000

    added policy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,17 +3,11 @@ class ReviewsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-<<<<<<< HEAD
     @request = Request.where(product: @product, user: current_user).first
     @review = Review.new(review_params)
-    @review.user = current_user
-    @review.request = @request
-=======
-    @request = Request.where(product: @product, user: current_user, status: "Accepted").first
-    @review = Review.new(review_params)
     @review.request = @request
     @review.user = current_user
->>>>>>> a7946678d18b282812da8896fd611327770ccaa2
+    authorize(@review)
     if @review.save
       redirect_to product_path(@product)
     else
