3d36ccc86060dad3b94bab6eb5b6fa58990a71f7
Vaiva Margeleviciute <vaivav11@gmail.com>
2017-11-20 17:05:09 +0000

    date picker and calendar are displaying but not linked yet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,12 @@
 //= require underscore
 //= require gmaps/google
 //= require algolia/v3/algoliasearch.min
+//= require jquery
+//= require moment
+//= require fullcalendar
+//= require bootstrap-datepicker
 //= require_tree .
+
 
 // var algoliasearch = require('algoliasearch');
 // var algoliasearch = require('algoliasearch/reactnative');
@@ -10,4 +15,16 @@
 // or just use algoliasearch if you are using a <script> tag
 // if you are using AMD module loader, algoliasearch will not be defined in window,
 // but in the AMD modules of the page
+
+$('document').ready(function(){
+      $('.datepicker').datepicker({
+        clearBtn: true,
+        todayHighlight: true,
+        startDate: new Date() + 1,
+        autoclose: true,
+        // datesdisabled : <%= @unavailabledates %>
+      });
+      $('#calendar').fullCalendar({});
+    });
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171117103218) do
+ActiveRecord::Schema.define(version: 20171120155533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20171117103218) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "start_date"
-    t.date "end_date"
+    t.string "start_date"
+    t.string "end_date"
     t.string "status", default: "Pending"
     t.index ["product_id"], name: "index_requests_on_product_id"
     t.index ["user_id"], name: "index_requests_on_user_id"
