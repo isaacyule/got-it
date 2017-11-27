c42d45935956629df7c7b13185907afe26ee5048
isaac yule <isaac.k.yule@gmail.com>
2017-11-27 10:17:38 +0000

    added rating column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124122642) do
+ActiveRecord::Schema.define(version: 20171127100229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(version: 20171124122642) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.float "rating", default: 0.0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
