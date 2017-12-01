9fd4222c49014fa2d1bdbaff8085f816cc0e2fcd
Robinson Coulton <robinsoncoulton@hotmail.co.uk>
2017-12-01 11:04:28 +0000

    added meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,4 +20,8 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)|(notifications)/
   end
+
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
 end
